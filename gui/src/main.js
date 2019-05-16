a100ab5a62fdb8988dac5443d43f11c47a24766e
Phil Schatzmann <phil.schatzmann@gmail.com>
2019-05-16 06:00:30 +0200

    on off switch

--- gui/src/main.js
+++ gui/src/main.js
@@ -12,3 +12,12 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
+// Check authorizaitons for each router step
+router.beforeEach((to, from, next) => {
+  console.log("Navigating to ", to.fullPath)
+
+  // clear errors when we change the screen
+  store.dispatch("setError", null)
+  store.dispatch("setProcessing", false)
+
+})
