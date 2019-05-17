32b4bf4840495c48a4ba1be04ca15a971a3fdebe
Phil Schatzmann <phil.schatzmann@gmail.com>
2019-05-17 21:28:10 +0200

    network statistics

--- gui/src/views/Performance.vue
+++ gui/src/views/Performance.vue
@@ -13,10 +13,10 @@
         <v-container id='performanceContainer' fluid>
             <v-card>
                 <v-container fluid>
-                    <area-chart :stacked="true" :data="chartData.rx" legend="right"  :title="'Traffic In '+unit"/>
+                    <area-chart :stacked="true" :data="chartData.rx" legend="right"  :title="'Traffic RX '+unit"/>
                 </v-container>
                 <v-container fluid>
-                    <area-chart :stacked="true" :data="chartData.tx" legend="right" :title="'Traffic Out '+unit"/>
+                    <area-chart :stacked="true" :data="chartData.tx" legend="right" :title="'Traffic TX '+unit"/>
                 </v-container>
             </v-card>
         </v-container>
@@ -36,7 +36,7 @@ export default {
         timer: null,
         asBytes: 'Bytes',
         chartData : {rx:[], tx: []},
-        unit: null
+        unit: ""
 
        // [ 
        //     {name: 'node1', data: {'2017-01-01 00:00:00 -0800': 3, '2017-01-02 00:00:00 -0800': 4}},
@@ -56,7 +56,7 @@ export default {
             // trigger reloading of data which will be diplayed with next call
             WebServices.getNetworkTraffic(this.asBytes == 'Bytes').then(result => {
                 this.chartData = result.data
-                this.unit = asBytes == 'Bytes' ? 'Bytes' : 'Packets'
+                this.unit = this.asBytes == 'Bytes' ? '(Bytes)' : '(Packets)'
             },error => {
                 this.$store.dispatch('setError', error)
             })
@@ -65,7 +65,7 @@ export default {
 
     created() {
         this.updateData();
-        this.timer = setInterval(this.updateData(), 10000)
+        this.timer = setInterval(() => this.updateData(), 5000)
     },
 
     beforeDestroy() {
